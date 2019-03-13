a040caec012d88c1da480efaaf846ed852dddb10
Ryan Abernathey <ryan.abernathey@gmail.com>
2019-03-13 16:30:35 -0400

    add import tests

---
+++ tests/test_imports.py
@@ -0,0 +1,14 @@
+def test_import_intake():
+    import intake
+
+def test_import_gcsfs():
+    import gcsfs
+
+def test_import_xarray():
+    import xarray
+
+def test_import_netCDF4():
+    import netCDF4
+
+def test_import_rasterio():
+    import rasterio
